21ab1c59d9820d2f5f0a2b048a09259ac22901e7
danilo <37438774+khalld@users.noreply.github.com>
2022-07-09 19:11:48 +0200

    Springfox configuration

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # sprecsample
+
+`launch.json`
+
+````
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "java",
+            "name": "Debug",
+            "request": "launch",
+            "mainClass": "sprec.sprecsample.SprecsampleApplication",
+        }
+    ]
+}
+````
+
+## Note
+
+Nelle properties inserire `host.docker.internal` se stiamo runanndo il microservizio su un container, altrimenti `localhost` se lo stiamo eseguendo in debug, ad esempio.
+Ciò è dato dal fatto che i due container non stanno sullo stesso localhost
---
+++ src/main/java/sprec/sprecsample/configuration/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package sprec.sprecsample.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+    private final String baseUrl = "http://localhost:9094";
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                // SWAGGER
+                .addViewController(baseUrl + "/swagger-ui/")
+                .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
+    }
+}
